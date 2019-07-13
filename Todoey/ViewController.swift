6be322386e0a6fda1005e8fb6b7bc500663c8741
Peter Götze <peter.goetze@me.com>
2019-07-13 17:07:32 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Peter Götze on 13.07.19| KW 28.
+//  Copyright © 2019 Peter Götze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
