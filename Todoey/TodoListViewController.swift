b6a25cbcb6f7d6aef226834ebd9404b8bfc30b66
Peter Götze <peter.goetze@me.com>
2019-08-12 15:46:00 +0200

    Add local Storage Array

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -52,6 +56,8 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item,", style: .default) { (action) in
                 // what will happen?
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
